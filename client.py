f67bad03c1d905a2c2b7a7fc1779f6d5993cdd3d
Amanara <52249849+4m4Sec@users.noreply.github.com>
2022-08-16 01:35:44 +0200

    update [1.0.0] : project complete

--- client.py
+++ client.py
@@ -1,1 +1,24 @@
 import socket
+import random
+import os
+
+os.system("clear")
+print("//~~ Client Side ~~//")
+
+host, port = ("127.0.0.1", 1337)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
+
+# Envoie de la requete SYN :
+m = str(random.randint(1000, 2000)) # Definition de l'id SYN
+sock.sendall(b"[1] SYN/seq=" + m.encode())
+
+# Reception de la requete SYN-ACK :
+synack = sock.recv(1024).decode()
+print(synack)
+
+# Envoie de la requete ACK :
+n = synack.split("seq=")[1].split(" ")[0] # Recuperation de l'id SYN (qui deviendra l'id ACK)
+n = str(int(n) + 1) # Ajout de 1 à l'id SYN
+sock.sendall(b"[3] ACK/seq=" + n.encode())
--- server.py
+++ server.py
@@ -1,1 +1,28 @@
 import socket
+import random
+import os
+
+os.system("clear")
+print("//~~ Server Side ~~//")
+
+host, port = ("127.0.0.1", 1337)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((host, port))
+sock.listen(1)
+
+(client, (host, port)) = sock.accept()
+
+# Reception de la requete SYN :
+syn = client.recv(1024).decode()
+print(syn)
+
+# Envoie de la requete SYN-ACK :
+m = syn.split("seq=")[1] # Recuperation de l'id SYN (qui deviendra l'id ACK)
+m = str(int(m) + 1) # Ajout de 1 à l'id SYN
+n = str(random.randint(1000, 2000)) # Definition de l'id SYN
+client.sendall(b"[2] SYN/seq=" + n.encode() + b" ACK/seq=" + m.encode())
+
+# Reception de la requete ACK :
+ack = client.recv(1024).decode()
+print(ack)
